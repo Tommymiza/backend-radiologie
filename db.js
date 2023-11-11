e0c2a903f9a145bdf5269163432994e9a1f1a16d
Tommymiza <tommymiza20@gmail.com>
2023-11-11 09:29:04 +0300

    Mysql

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectTimeout: 10000,
     waitForConnections: true,
     debug: true,
     ssl: {

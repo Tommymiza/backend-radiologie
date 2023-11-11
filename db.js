a4c3b4467873b280c2b45393691801020ddf19bc
Tommymiza <tommymiza20@gmail.com>
2023-11-11 09:26:06 +0300

    Mysql

--- db.js
+++ db.js
@@ -3,7 +3,15 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectTimeout: 10000,
+    waitForConnections: true,
+    connectionLimit: 100,
+    multipleStatements: true,
+    idleTimeout: 10000,
+    maxIdle: 10000,
+    debug: true,
+    maxPreparedStatements: 100,
 });
 
 module.exports = db;

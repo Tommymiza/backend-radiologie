5f89ebb06c6faf91a7953ac7aa566b3961b5bb36
Tommymiza <tommymiza20@gmail.com>
2023-11-11 15:35:27 +0300

    DB

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   pool: {
+    handleDisconnect: ()=>{
+        return db;
+    },
     max: 10,
     min: 0,
     acquire: 30000,
@@ -12,24 +15,5 @@ const db = mysql.createConnection({
   },
   port: 3306,
 });
-function handleDisconnect() {
-  db.connect(function (err) {
-    if (err) {
-      console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  });
-  db.on("error", function (err) {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      // Connection to the MySQL server is usually
-      handleDisconnect(); // lost due to either server restart, or a
-    } else {
-      // connnection idle timeout (the wait_timeout
-      throw err; // server variable configures this)
-    }
-  });
-}
-handleDisconnect();
 
 module.exports = db;
